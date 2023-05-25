91ced90f8c7ecf23de9356fbe643b449075e7c52
Emily <erpegueromarmolejos@gmail.com>
2023-05-25 13:07:26 -0400

    improve readability

--- src/MonteCarlo.java
+++ src/MonteCarlo.java
@@ -1,18 +1,13 @@
-import java.util.Random;
-
 public class MonteCarlo {
-	
-	public static double random(double low, double high) {//produces a random double between low and high
-		Random random = new Random();
-		double result=0.0;
-		while (true) {
-			result = low + (random.nextDouble() * (high - low));
-			if(result != 0.0) break;
-		}
-		return result;
+	//creating a random number generator
+	public static double randomVal(double low, double high) {
+		//produces a random double between low and high
+		return low + ((high - low)*Math.random());
 	}
 	
-	public static double f(double x) {//y = f(x) = 3x^4-10x
+	//defining our mathematical function
+	public static double f(double x) {
+		//y = f(x) = 3x^4-10x
 		double expr1 = 3*x*x*x*x;
 		double expr2 = 10*x;
 		return (expr1-expr2);
@@ -23,18 +18,18 @@ public class MonteCarlo {
 		double low = 0.0;
 		double high = 5.0;
 		
-		for(int i = 0; i < 99000; i++){// the more iterations the higher the accuracy
-			double tempx = random(-1, 6); // getting a random value of x between 0 and 5
-			double y = f(tempx);// getting a random y value from the previously created random x value and inserting it into the function
-			double tempy = random ((int)(f(-1)), (int)(f(6)));// getting the random value of y between f(0) and f(5)
-			if(y != 0) {// weather y value generated from f(x) is greater than or less than 0, it will be added to the result.
+		// the more iterations the higher the accuracy
+		for(int i = 0; i < 99000; i++){
+			//double tempx = ; // 
+			double y = f(randomVal(low, high));// getting a random value of x between 0 and 5 and inserting it into the function to obtain a sample value of y 
+			//double tempy = randomVal(f(low), f(high));// getting the random value of y between f(0) and f(5)
+			if(y != 0 ) {// weather y value generated from f(x) is greater than or less than 0, it will be added to the result.
 				result +=y;
 			}
-			if(y<tempy) {
-				result -=y;
-			}
 		}
-		double diff = high - low;
-		System.out.println("The area of the integral is about " + ((result*diff)/99000));// because there are 99,000 iterations
-	}
+		double monteCarloResult = (((high - low)*result)/99000);
+		double actualIntegral = f(5) - f(0);
+		System.out.println("The area of the integral is about " + monteCarloResult);// because there are 99,000 iterations
+		System.out.println("The percentage error is " + (Math.abs(monteCarloResult - actualIntegral)/ actualIntegral) * 100);
+	};
 }
